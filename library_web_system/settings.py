eb1106d86be9a2451a0c28362cf9ad3c3111b04d
Charmaine Cator <xxiimcha@users.noreply.github.com>
2024-04-30 20:10:04 +0800

    update file name and generated secret key

--- library_web_system/settings.py
+++ library_web_system/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("KEY")
+SECRET_KEY = 'l0yw5&ox2!+68*lx#*6w5uev_()njueg%q$8n_!cnyi&nf8fd2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEV")
