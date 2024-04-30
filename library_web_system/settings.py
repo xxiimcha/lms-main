975d41a09771a8165873a5b0a8fbf6201f6dca7a
Charmaine Cator <xxiimcha@users.noreply.github.com>
2024-04-30 20:15:28 +0800

    update settings

--- library_web_system/settings.py
+++ library_web_system/settings.py
@@ -26,13 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l0yw5&ox2!+68*lx#*6w5uev_()njueg%q$8n_!cnyi&nf8fd2'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'l0yw5&ox2!+68*lx#*6w5uev_()njueg%q$8n_!cnyi&nf8fd2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEV")
+DEBUG = False
 
-ALLOWED_HOSTS = ['librarymanagement.online, www.librarymanagement.online']
-
+ALLOWED_HOSTS = ['librarymanagement.online', 'www.librarymanagement.online']
 
 # Application definition
 
